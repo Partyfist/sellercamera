93e990aefc182a1b194488c8737cf4f3b9da3bf0
Sungning <sungning@SungningdeMacBook-Air.local>
2026-03-29 20:55:22 +0800

    init SellerCamera project

---
+++ SellerCamera/SellerCamera/SellerCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SellerCameraApp.swift
+//  SellerCamera
+//
+//  Created by Sungning on 2026/3/29.
+//
+
+import SwiftUI
+
+@main
+struct SellerCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
